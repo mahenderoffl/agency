57a5c7e6206564e809ec6c311a38d65823e51c56
BANOTH MAHENDER <mahender_2311res32@iitp.ac.in>
2026-01-31 21:15:01 +0000

    chore: add missing React app structure for Netlify deployment

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WaveSeedGrowthLanding from "./app/WaveSeedGrowthLanding";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <WaveSeedGrowthLanding />
+  </React.StrictMode>
+);
